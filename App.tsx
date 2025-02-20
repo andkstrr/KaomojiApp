fbd0f17b9a8dee3667d2860c6a15e552fbbe0fbb
andkstrr <andikasatrionurcahyo@gmail.com>
2025-02-20 14:16:58 +0700

    KaomojiApp

--- App.tsx
+++ App.tsx
@@ -1,131 +1,87 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useState, useEffect } from 'react'
+import { View, Text, StyleSheet, StatusBar, FlatList, TouchableOpacity } from 'react-native'
+import Clipboard from '@react-native-clipboard/clipboard'
+// import Icon from 'react-native-vector-icons/FontAwesome'
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+  // Kaomoji
+  const [dataKaomoji, setDataKaomoji] = useState([
+    ' ˶ᵔ ᵕ ᵔ˶ ',
+    '₍ᐢ.  ̫.ᐢ₎',
+    'ദ്ദി •⩊• )',
+    'ଘ(੭ˊᵕˋ)੭',
+    '(˶ˆᗜˆ˵)',
+    '૮ ˙Ⱉ˙ ა rawr!    ',
+    '૮₍˶ •. • ⑅₎ა ♡',
+    '૮꒰˶> ᴗ <˶꒱ა',
+    '૮ ˶ᵔ ᵕ ᵔ˶ ა',
+    '⸜(｡˃ ᵕ ˂ )⸝♡',
+    '૮₍ ˃ ⤙ ˂ ₎ა',
+    
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  ])
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the reccomendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
+  function copyKaomoji(kaomoji: string) {
+    Clipboard.setString(kaomoji)
+  }
 
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <View style={ styles.container }>
+       {/* StatusBar */}
+       <StatusBar backgroundColor={'#282c34'} barStyle={'light-content'} />
+       
+       <View style={ styles.header }>
+          {/* <Icon name="rocket" size={30} color="#900" /> */}
+          <Text style={ styles.textHeader }>Kaomoji ^^</Text>
+       </View>
+      
+       {/* FlatList Kaomoji */}
+       <FlatList 
+          data={ dataKaomoji }
+          // identifikasi item unik
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <View style={ styles.bgKaomoji }>
+              <TouchableOpacity onPress={() => copyKaomoji(item)}>
+                <Text style={ styles.textKaomoji }>{ item }</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        />
     </View>
-  );
+  )
 }
+
+export default App
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#21252b'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  header: {
+    backgroundColor: '#282c34',
+    paddingVertical: 20,
+    paddingLeft: 20
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  textHeader: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+    elevation: 3 // sedikit shadow
   },
-  highlight: {
-    fontWeight: '700',
+  textKaomoji: {
+    color: '#fff',
+    alignItems: 'center',
+    paddingLeft: 35,
+    fontSize: 40,
   },
-});
-
-export default App;
+  bgKaomoji: {
+    paddingVertical: 20,
+    marginVertical: 10,
+    marginHorizontal: 20,
+    backgroundColor: '#282c34',
+    borderRadius: 10,
+    elevation: 3 // sedikit shadow
+  }
+})
